e4e0ee5e4d4c8fe100bf8e3e408c819d7bf73ef7
23MH1A05E5 <147487915+23MH1A05E5@users.noreply.github.com>
2023-10-31 20:27:47 +0530

    #include<stdio.h> int main() {     int x,y,c;     scanf("%d%d",&x,&y);     c=x-y;     if(x>y)     {         printf("%d",c);     }     else     {         printf("%d",c);     } }

---
+++ Chairs_Requirement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,c;
+    scanf("%d%d",&x,&y);
+    c=x-y;
+    if(x>y)
+    {
+        printf("%d",c);
+    }
+    else
+    {
+        printf("%d",c);
+    }
+}
